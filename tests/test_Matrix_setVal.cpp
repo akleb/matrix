0b6d2146fd2d01dc0719fcf4b01d0909adad4d65
Alex Kleb <akleb@umich.edu>
2021-10-20 13:48:06 -0400

    add Matrix_setVal test

---
+++ tests/test_Matrix_setVal.cpp
@@ -0,0 +1,33 @@
+/*
+    Alex Kleb
+
+    Test to check Matrix initialization
+*/
+
+#include "matrix.hpp"
+
+#ifndef D_TYPE
+#define D_TYPE int
+#endif
+
+int main(){
+
+    int i, j, n = 3, m = 2;
+    D_TYPE val = 2;
+    D_TYPE set = -2;
+
+    Matrix<D_TYPE> mat(n, m);
+
+    for(i = 0; i < n; ++i){
+        for(j = 0; j < m; ++j){
+            mat.setVal(i, j, set);
+            mat.getVal(i, j, &val);
+            if (val != -2) return 1;
+            val = 0;
+        } //for
+    } //for
+
+
+    return 0;
+
+} //main
